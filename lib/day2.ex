37fdf1dc3c28559f7636cf008234a41c60bf48e5
Fabrice CLAEYS <fabrice.claeys@symbol-it.fr>
2022-12-02 21:46:49 +0100

    online methods

--- lib/day2.ex
+++ lib/day2.ex
@@ -24,75 +24,27 @@ defmodule AdventOfCode2022.Day2 do
     |> Enum.reduce(0, fn {opp, guess}, acc -> acc + score(opp, guess) end)
   end
 
-  defp choose_guess("A", "X") do
-    "Z"
-  end
+  defp choose_guess("A", "X"), do: "Z"
+  defp choose_guess("A", "Y"), do: "X"
+  defp choose_guess("A", "Z"), do: "Y"
 
-  defp choose_guess("A", "Y") do
-    "X"
-  end
+  defp choose_guess("B", "X"), do: "X"
+  defp choose_guess("B", "Y"), do: "Y"
+  defp choose_guess("B", "Z"), do: "Z"
 
-  defp choose_guess("A", "Z") do
-    "Y"
-  end
+  defp choose_guess("C", "X"), do: "Y"
+  defp choose_guess("C", "Y"), do: "Z"
+  defp choose_guess("C", "Z"), do: "X"
 
-  defp choose_guess("B", "X") do
-    "X"
-  end
+  defp score("A", "X"), do: 1 + 3
+  defp score("A", "Y"), do: 2 + 6
+  defp score("A", "Z"), do: 3 + 0
 
-  defp choose_guess("B", "Y") do
-    "Y"
-  end
+  defp score("B", "X"), do: 1 + 0
+  defp score("B", "Y"), do: 2 + 3
+  defp score("B", "Z"), do: 3 + 6
 
-  defp choose_guess("B", "Z") do
-    "Z"
-  end
-
-  defp choose_guess("C", "X") do
-    "Y"
-  end
-
-  defp choose_guess("C", "Y") do
-    "Z"
-  end
-
-  defp choose_guess("C", "Z") do
-    "X"
-  end
-
-  defp score("A", "X") do
-    1 + 3
-  end
-
-  defp score("A", "Y") do
-    2 + 6
-  end
-
-  defp score("A", "Z") do
-    3 + 0
-  end
-
-  defp score("B", "X") do
-    1 + 0
-  end
-
-  defp score("B", "Y") do
-    2 + 3
-  end
-
-  defp score("B", "Z") do
-    3 + 6
-  end
-
-  defp score("C", "X") do
-    1 + 6
-  end
-
-  defp score("C", "Y") do
-    2 + 0
-  end
-
-  defp score("C", "Z") do
-    3 + 3
-  end
+  defp score("C", "X"), do: 1 + 6
+  defp score("C", "Y"), do: 2 + 0
+  defp score("C", "Z"), do: 3 + 3
 end
