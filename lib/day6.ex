8701be1f1b536e060db47affe456312dcd03c146
Fabrice CLAEYS <fabrice.claeys@symbol-it.fr>
2022-12-15 13:29:41 +0100

    solve day6

--- lib/day6.ex
+++ lib/day6.ex
@@ -2,14 +2,22 @@ defmodule AdventOfCode2022.Day6 do
   def tuning_trouble_1() do
     File.read!(Path.join("data", "day6_1.txt"))
     |> String.graphemes()
-    |> analyse_datastream(0)
+    |> analyse_datastream_1(4)
   end
 
-  defp analyse_datastream(datastream, cpt) do
+  def tuning_trouble_2() do
+    File.read!(Path.join("data", "day6_1.txt"))
+    |> String.graphemes()
+    |> analyse_datastream_2(14)
+  end
+
+  defp analyse_datastream_1(datastream, cpt) do
     characters =
       datastream
       |> Enum.chunk_every(4)
       |> Enum.take(1)
+
+    IO.inspect(characters)
 
     case is_maker(characters) do
       {true, char_count} ->
@@ -17,7 +25,43 @@ defmodule AdventOfCode2022.Day6 do
 
       {false, _} ->
         [_ | to_analyse] = datastream
-        analyse_datastream(to_analyse, cpt + 4)
+        analyse_datastream_1(to_analyse, cpt + 1)
+    end
+  end
+
+  defp analyse_datastream_2(datastream, cpt) do
+    characters =
+      datastream
+      |> Enum.chunk_every(14)
+      |> Enum.take(1)
+
+    IO.inspect(characters)
+
+    case is_maker(characters) do
+      {true, char_count} ->
+        cpt + char_count
+
+      {false, _} ->
+        [_ | to_analyse] = datastream
+        analyse_datastream_2(to_analyse, cpt + 1)
+    end
+  end
+
+  defp is_maker([characters]) when length(characters) == 14 do
+    result =
+      characters
+      |> Enum.group_by(fn c -> c end)
+      |> Map.values()
+      |> Enum.filter(fn x -> length(x) > 1 end)
+
+    # IO.inspect(result)
+
+    if length(result) > 0 do
+      {false, 1}
+    else
+      # trouver le dernier index du caractere qui se repete
+
+      {true, 0}
     end
   end
 
@@ -28,12 +72,14 @@ defmodule AdventOfCode2022.Day6 do
       |> Map.values()
       |> Enum.filter(fn x -> length(x) > 1 end)
 
-    if length(result) > 0 do
-      {false, 4}
-    else
-      [c | _] = result
+    # IO.inspect(result)
 
-      {true, Enum.find_index(characters, fn char -> char == c end)}
+    if length(result) > 0 do
+      {false, 1}
+    else
+      # trouver le dernier index du caractere qui se repete
+
+      {true, 0}
     end
   end
 end
